ee5fe734a1bb979bf2282d92e126e7e305bf3cb4
meep-morp <gracieaerogirl@gmail.com>
2020-04-20 15:42:08 -0500

    Finished task 2

--- index.js
+++ index.js
@@ -24,10 +24,12 @@ fifaData.forEach(function(item){
 /* Task 2: Create a function called  getFinals that takes `data` as an argument and returns an array of objects with only finals data */
 
 function getFinals(data) {
-
-    /* code here */
-
+    const finalsData = data.filter(function(item){
+      return item.Stage === "Final";
+    });
+    return finalsData;
 };
+console.log(getFinals(fifaData));
 
 /* Task 3: Impliment a higher-order function called `getYears` that accepts the callback function `getFinals`, and returns an array called `years` containing all of the years in the dataset */
 
